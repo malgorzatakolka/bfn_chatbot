fe4c451dd47b3b72db402548d35bc028bb4b8b79
Malgorzata Kolka <117841097+malgorzatakolka@users.noreply.github.com>
2024-09-24 11:06:43 +0100

    into bfn bfn_app.py

--- bfn_app.py
+++ bfn_app.py
@@ -4,17 +4,13 @@ from snowflake.core import Root
 import pandas as pd
 import json
 
-pd.set_option("max_colwidth", None)
-
 NUM_CHUNKS = 3
-
-
 CORTEX_SEARCH_DATABASE = "BFN_PROJECT"
 CORTEX_SEARCH_SCHEMA = "DATA"
 CORTEX_SEARCH_SERVICE = "CC_SEARCH_SERVICE_CS"
 
 
-# columns to query in the service
+# Columns to query in the search service
 COLUMNS = [
     "chunk",
     "relative_path",
@@ -22,18 +18,10 @@ COLUMNS = [
     "category"
 ]
 
-# Establish Snowflake session
-
+# Establish Snowflake connection
 cnx = st.connection('snowflake')
-
-
 session = cnx.session()
 root = Root(session)
-
-
-
-                        
-
 svc = root.databases[CORTEX_SEARCH_DATABASE].schemas[CORTEX_SEARCH_SCHEMA].cortex_search_services[CORTEX_SEARCH_SERVICE]
    
 ### Functions
@@ -50,16 +38,14 @@ def config_options():
                                      'mistral-7b',
                                      'llama2-70b-chat',
                                      'gemma-7b'), key="model_name")
-
+    # Creating category selectbox
     categories = session.table('docs_chunks_table').select('category').distinct().collect()
-
     cat_list = ['ALL']
     for cat in categories:
         cat_list.append(cat.CATEGORY)
-            
-
     st.sidebar.selectbox('Select what products you are looking for', cat_list, key = "category_value")
 
+    # Session state for debugging purposes
     st.sidebar.expander("Session State").write(st.session_state)
 
 def get_similar_chunks_search_service(query):
@@ -71,50 +57,39 @@ def get_similar_chunks_search_service(query):
         response = svc.search(query, COLUMNS, filter=filter_obj, limit=NUM_CHUNKS)
 
     st.sidebar.json(response.json())
-    
     return response.json()  
 
 def create_prompt (myquestion):
 
-    if st.session_state.rag == 1:
-        prompt_context = get_similar_chunks_search_service(myquestion)
-  
-        prompt = f"""
-           You are an expert chat assistance that extracs information from the CONTEXT provided
-           between <context> and </context> tags.
-           When ansering the question contained between <question> and </question> tags
-           be concise and do not hallucinate. 
-           If you don´t have the information just say so.
-           Only anwer the question if you can extract it from the CONTEXT provideed.
-           
-           Do not mention the CONTEXT used in your answer.
-    
-           <context>          
-           {prompt_context}
-           </context>
-           <question>  
-           {myquestion}
-           </question>
-           Answer: 
-           """
+    prompt_context = get_similar_chunks_search_service(myquestion)
 
-        json_data = json.loads(prompt_context)
+    prompt = f"""
+       You are an expert chat assistance that extracs information from the CONTEXT provided
+       between <context> and </context> tags.
+       When ansering the question contained between <question> and </question> tags
+       be concise and do not hallucinate. 
+       If you don´t have the information just say so.
+       Only anwer the question if you can extract it from the CONTEXT provideed.
+       
+       Do not mention the CONTEXT used in your answer.
 
-        relative_paths = set(item['relative_path'] for item in json_data['results'])
-        
-    else:     
-        prompt = f"""[0]
-         'Question:  
-           {myquestion} 
-           Answer: '
-           """
-        relative_paths = "None"
-            
-    return prompt, relative_paths
+       <context>          
+       {prompt_context}
+       </context>
+       <question>  
+       {myquestion}
+       </question>
+       Answer: 
+       """
+
+    json_data = json.loads(prompt_context)
+
+    linked_url = set(item['linked_url'] for item in json_data['results'])
+    return prompt, linked_url
 
 def complete(myquestion):
 
-    prompt, relative_paths =create_prompt (myquestion)
+    prompt, linked_url = create_prompt(myquestion)
     cmd = """
             select snowflake.cortex.complete(?, ?) as response
           """
@@ -124,34 +99,37 @@ def complete(myquestion):
 
 def main():
     
-    st.title(f":speech_balloon: Chat Document Assistant with Snowflake Cortex")
-    st.write("This is the list of documents you already have and that will be used to answer your questions:")
-    docs_available = session.sql("ls @docs").collect()
-    list_docs = []
-    for doc in docs_available:
-        list_docs.append(doc["name"])
-    st.dataframe(list_docs)
+    css = """
+    <style>
+    .seledin-title {
+        color: #32CD32;  /* Seledin color (LimeGreen in CSS color names) */
+    }
+    </style>
+    """
+
+    # Inject the CSS into the app
+    st.markdown(css, unsafe_allow_html=True)
+
+    # Use st.markdown to display the title with the custom class
+    st.markdown('<h1 class="seledin-title">Breastfeeding Network Drug in Breastmilk Document Assistant</h1>', unsafe_allow_html=True)
+    
+    st.write('''<p style="color:purple;">The information provided is taken from various reference source. It is provided as a guideline.
+             No responsibility can be taken by the author or BfN for the way in which the information is used. 
+             Clinical decisions remain the responsibility of medical and breastfeeding practitioners. The data presented here is intended to provide some
+             information but cannot replace input from professionals</p>''', unsafe_allow_html=True
+             )
+
 
     config_options()
-
-    st.session_state.rag = st.sidebar.checkbox('Use your own documents as context?')
 
     question = st.text_input("Enter question", placeholder="Is there any special lubricant to be used with the premium bike?", label_visibility="collapsed")
 
     if question:
-        response, relative_paths = complete(question)
+        response, linked_paths = complete(question)
         res_text = response[0].RESPONSE
-        st.markdown(res_text)
+        display_url = f"Doc: [{url_link}]({url_link})"
+        st.markdown(res_text, display_url)
 
-        if relative_paths != "None":
-            with st.sidebar.expander("Related Documents"):
-                for path in relative_paths:
-                    cmd2 = f"select GET_PRESIGNED_URL(@docs, '{path}', 360) as URL_LINK from directory(@docs)"
-                    df_url_link = session.sql(cmd2).to_pandas()
-                    url_link = df_url_link._get_value(0,'URL_LINK')
-        
-                    display_url = f"Doc: [{path}]({url_link})"
-                    st.sidebar.markdown(display_url)
                 
 if __name__ == "__main__":
     main()
